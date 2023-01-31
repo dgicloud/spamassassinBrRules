5ee9e6df9793de3104ffc99ef184cc248d60bf50
dgicloud <carlos@dgicloud.com.br>
2023-01-31 19:29:57 -0300

    Create cpanel_run.sh

---
+++ cpanel_run.sh
@@ -0,0 +1,17 @@
+#chegando se há atuaização no repositorio
+cd /root/spamassassinBrRules/ && git pull
+
+#backup do arquivo atual
+cp /etc/mail/spamassassin/custom.cf /etc/mail/spamassassin/custom.cf-bkp-`date +%Y-%m-%d-%H%M`.old
+
+#Copie o novo arquivo para o custom.cf
+cp spamassassin-custom.cf /etc/mail/spamassassin/custom.cf
+
+#reiniciaando o o spamd
+/scripts/restartsrv_spamd 
+
+
+
+
+
+
